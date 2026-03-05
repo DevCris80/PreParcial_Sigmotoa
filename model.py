3b77077835e9a5baaa971a0e7f0ae8c914119070
DevCris80 <cristian.steven05@gmail.com>
2026-03-05 15:28:07 -0500

    Metodos de pokemon

--- model.py
+++ model.py
@@ -7,6 +7,16 @@ class Pokemon(BaseModel):
     life: float
     type: str
 
+    def leavePokeball(self) -> str:
+        return f"{self.name}, Yo te elijo"
+
+    def attack(self, target_pokemon: "Pokemon") -> str:
+        target_pokemon.life -= self.attack
+        target_pokemon.life = max(0, target_pokemon.life) 
+        
+        return f"{self.name} ataca a {target_pokemon.name}. Vida restante de {target_pokemon.name}: {target_pokemon.life}"
+
+
 class MessageBattle(BaseModel):
     turno: int
     mensaje: str
